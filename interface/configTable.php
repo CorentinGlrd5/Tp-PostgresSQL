916341b50b824e29c35379a06f00c40b6a71eb43
leorigaudeau <leo.rigaudeau@ynov.com>
2019-04-05 09:42:53 +0200

    CSS Léo

--- interface/configTable.php
+++ interface/configTable.php
@@ -5,30 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/creatTable.css" />
 </head>
 <body>
     <h2>Choissisez le schema ou vous voulez créer la table :</h2>
-    <?php
-    session_start();
-    try{
-        $conn = new PDO($_SESSION['cledsn']);
-       }catch (PDOException $e){
-        // report error message
-       echo "Database is not available :(";
-       }
-       echo '<form action="creaTable.php" method="post">';
-       $pdoStat = $conn->prepare('SELECT distinct schema_name FROM information_schema.schemata');
-       $executeIsOK = $pdoStat->execute();
-       $selectsche = $pdoStat->fetchAll();
+    <div class="middl">
+        <div class="start">
+            <?php
+            session_start();
+            try{
+                $conn = new PDO($_SESSION['cledsn']);
+            }catch (PDOException $e){
+                // report error message
+            echo "Database is not available :(";
+            }
+            echo '<form action="creaTable.php" method="post">';
+            $pdoStat = $conn->prepare('SELECT distinct schema_name FROM information_schema.schemata');
+            $executeIsOK = $pdoStat->execute();
+            $selectsche = $pdoStat->fetchAll();
 
-       foreach ($selectsche as $value) {    
-        echo '<input type="checkbox" name="schema"  value="'.$value["schema_name"].'"> '.$value['schema_name'];
-        echo '<br>';       
-       }
-    ?>
-        <input type="text" placeholder="Entrez le nombre de colonne" name="nbcolone"> 
-        <input type="submit" value=" Crée"/>
-     </form>
+            foreach ($selectsche as $value) {    
+                echo '<input type="checkbox" name="schema"  value="'.$value["schema_name"].'"> '.$value['schema_name'];
+                echo '<br>';       
+            }
+            ?>
+                <input type="text" placeholder="Entrez le nombre de colonne" name="nbcolone"> 
+                <input type="submit" value=" Crée"/>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
--- interface/creaSchema.php
+++ interface/creaSchema.php
@@ -6,61 +6,66 @@
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../css/creatTable.css" />
 </head>
 <body>
     <h1>Création d'un nouveau schéma</h1>
-    <form action="" method="post">
-        <input type="text" name="namesch">
-        <input type="submit" value="crée un schema"/>
-    </form>
-    <h2>Liste des schema deja crée</h2>
-    <ul>
-    <?php
-    session_start();
-    try{
-        $conn = new PDO($_SESSION['cledsn']);
-       }catch (PDOException $e){
-        // report error message
-       echo "Database is not available :(";
-       }
-
-    if (isset($_POST["namesch"])&& $_POST["namesch"]!=" ") {
-        var_dump($_POST["namesch"]);
-        $pdoStat = $conn->prepare('SELECT distinct schema_name FROM information_schema.schemata');
-        $executeIsOK = $pdoStat->execute();
-        $selectsche = $pdoStat->fetchAll();
-
-        foreach ($selectsche as $value) {           
-            echo'<li>'.$value["schema_name"].'</li>';
-            if ($value["schema_name"]===$_POST["namesch"]) {
-                echo '<script type="text/javascript">window.alert("Ce nom est deja utiliser veuillez recommencer !");</script>';
-                $nameOK=false;
-                break;
-            }
-            $nameOK=true;
-        }
-        if($nameOK){
+    <div class="middl">
+        <div class="start">
+            <form action="" method="post">
+                <input type="text" name="namesch" placeholder="Nom du schéma">
+                <input type="submit" value="crée un schéma"/>
+            </form>
+            <h2>Liste des schema deja crée</h2>
+            <ul>
+            <?php
+            session_start();
             try{
-                $pdoStatCreatSche = $conn->prepare('CREATE SCHEMA '.$_POST["namesch"]);
-                $executeIsOKforsche = $pdoStatCreatSche->execute();
-                echo '<script type="text/javascript">window.alert("nous n\'avons pas trouvé de correspondanse avec un schema deja existant alors nous avons créer '.$_POST["namesch"].'");</script>';
-            }catch(PDOException $e){
+                $conn = new PDO($_SESSION['cledsn']);
+            }catch (PDOException $e){
                 // report error message
-               echo "exception";
+            echo "Database is not available :(";
             }
-        }
-    }
-    else {
-        $pdoStat = $conn->prepare('SELECT distinct schema_name FROM information_schema.schemata');
-        $executeIsOK = $pdoStat->execute();
-        $selectsche = $pdoStat->fetchAll();
 
-        foreach ($selectsche as $value) {
-            echo'<li>'.$value["schema_name"].'</li>'; 
-        }
-        echo " <h2>Veulliez créer un schema valide ! </h2>";
-    }
-    ?>
-    </ul>
+            if (isset($_POST["namesch"])&& $_POST["namesch"]!=" ") {
+                var_dump($_POST["namesch"]);
+                $pdoStat = $conn->prepare('SELECT distinct schema_name FROM information_schema.schemata');
+                $executeIsOK = $pdoStat->execute();
+                $selectsche = $pdoStat->fetchAll();
+
+                foreach ($selectsche as $value) {           
+                    echo'<li>'.$value["schema_name"].'</li>';
+                    if ($value["schema_name"]===$_POST["namesch"]) {
+                        echo '<script type="text/javascript">window.alert("Ce nom est deja utiliser veuillez recommencer !");</script>';
+                        $nameOK=false;
+                        break;
+                    }
+                    $nameOK=true;
+                }
+                if($nameOK){
+                    try{
+                        $pdoStatCreatSche = $conn->prepare('CREATE SCHEMA '.$_POST["namesch"]);
+                        $executeIsOKforsche = $pdoStatCreatSche->execute();
+                        echo '<script type="text/javascript">window.alert("nous n\'avons pas trouvé de correspondanse avec un schema deja existant alors nous avons créer '.$_POST["namesch"].'");</script>';
+                    }catch(PDOException $e){
+                        // report error message
+                    echo "exception";
+                    }
+                }
+            }
+            else {
+                $pdoStat = $conn->prepare('SELECT distinct schema_name FROM information_schema.schemata');
+                $executeIsOK = $pdoStat->execute();
+                $selectsche = $pdoStat->fetchAll();
+
+                foreach ($selectsche as $value) {
+                    echo'<li>'.$value["schema_name"].'</li>'; 
+                }
+                echo " <h2>Veulliez créer un schema valide ! </h2>";
+            }
+            ?>
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
